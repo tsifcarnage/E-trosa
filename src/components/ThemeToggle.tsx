f558a8472f060df4387ee256cb9da15c99422dbd
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-03-15 23:16:34 +0100

    Update ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,7 +5,7 @@ function ThemeToggle() {
             <input type="checkbox" className="theme-controller" value="winter" />
             {/* sun icon */}
             <svg
-                className="swap-off h-10 w-10 fill-current"
+                className="swap-on h-10 w-10 fill-current text-amber-400"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24">
                 <path
@@ -14,7 +14,7 @@ function ThemeToggle() {
 
             {/* moon icon */}
             <svg
-                className="swap-on h-10 w-10 fill-current"
+                className="swap-off h-10 w-10 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24">
                 <path
