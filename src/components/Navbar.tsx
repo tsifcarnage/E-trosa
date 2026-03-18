f6df621737a122ce6514e10f349bfb4a9ef21375
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-03-18 18:18:38 +0100

    Update Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import ThemeToggle from "./ThemeToggle";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <h3></h3>
+            <div>
+                <ThemeToggle/>
+                
+            </div>
+        </nav>
+    )
+}
