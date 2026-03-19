d52b5b65c389b4e3bf84d5c73f9083f6800c9c4e
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-03-19 13:06:32 +0100

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import { useLocation } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 import type { SidebarProps } from "../models/interfaces";
+import { FaRegUserCircle } from "react-icons/fa";
 
 export default function Navbar({ links }: SidebarProps) {
     const location = useLocation();
@@ -11,10 +12,11 @@ export default function Navbar({ links }: SidebarProps) {
     return (
         <nav className="flex justify-between p-4">
             <h3 className="grow">{currentLink?.label || ""}</h3>
-            <section className="ml-auto">
+            <section className="ml-auto flex gap-2">
                 <ThemeToggle />
-                <div>
-
+                <div className="flex gap-2 bg-neutral rounded-full p-2 text-[#c9cbd0] box-shad-user">
+                    <FaRegUserCircle size={40} />
+                    <h3 className="self-center">User Name</h3>
                 </div>
             </section>
         </nav>
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -14,7 +14,7 @@ function ThemeToggle() {
 
             {/* moon icon */}
             <svg
-                className="swap-off h-10 w-10 fill-current"
+                className="swap-off h-10 w-10 fill-current "
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24">
                 <path
