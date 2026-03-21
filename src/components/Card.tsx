9c4ec25067b99ffe1447c265678c801325b7ede6
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-03-21 22:08:57 +0100

    update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,19 @@
 import type { ICardGrad } from "../models/ui.interfaces"
 
 const cardGrad: ICardGrad[] = [
-    { title: "total à payer", label: 9000.00, unit: "€", color: "text-red-500", grad: "red-card-grad" },
-    { title: "total à recevoir", label: 9000.00, unit: "€", color: "text-green-500", grad: "green-card-grad" },
-    { title: "total à payer", label: 9000.00, unit: "€", color: "text-green-500", grad: "green-card-grad" },
+    { title: "Total à payer", label: 9000.00, unit: "€", color: "text-red-500", grad: "red-card-grad" },
+    { title: "Total à recevoir", label: 9000.00, unit: "€", color: "text-green-500", grad: "green-card-grad" },
+    { title: "Solde net", label: 9000.00, unit: "€", color: "text-orange-500", grad: "orange-card-grad" },
     { title: "total à payer", label: 9000.00, unit: "€", color: "text-red-500", grad: "red-card-grad" },
 ]
 export default function Card() {
     return (
-        (<div className="flex flex-col gap-2 mx-5 p-6 green-card-grad w-60 rounded-[10px]">
-            <h4 className="text-amber-50">Total à payer</h4>
-            <h3 className="text-green-500">9000.00 €</h3>
-        </div>)
+        <div className="flex gap-5 justify-center flex-wrap">
+            {cardGrad.map((c, index) => (<div className={`flex flex-col gap-2 mx-5 p-6 w-60 rounded-[10px] ${c.grad}`} key={index}>
+                <h4 className="text-amber-50">{c.title}</h4>
+                <h3 className={c.color}>{c.label} <span>{c.unit}</span></h3>
+            </div>))
+            }
+        </div>
     )
 }
