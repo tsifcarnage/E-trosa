1b2b38abd5c7de4b0bf21a3997ec71b271208596
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-03-23 12:02:19 +0100

    update

--- src/components/CoinAnimation.tsx
+++ src/components/CoinAnimation.tsx
@@ -2,7 +2,7 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 export default function CoinAnimation() {
     return (
         <DotLottieReact
-            src="/lottie/e-trosa-coin.json"
+            src="/lottie/e-trosa-coin.lottie"
             loop={true}
             autoplay={true}
             style={{ width: '200px', height: '200px' }}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,19 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 
+// filtre resoudre animation erreur
+const originalError = console.error;
+console.error = (...args: unknown[]) => {
+  const firstArg = args[0];
+  if (
+    typeof firstArg === "string" &&
+    firstArg.includes("Failed to load animation with id")
+  ) {
+    return;
+  }
+  originalError(...(args as Parameters<typeof console.error>));
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
