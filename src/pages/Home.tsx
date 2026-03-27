ffb21b66e6f92004413b6bed9042c0ac024eda55
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-03-27 19:36:07 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ function Home() {
     return (
         <div className="flex flex-col justify-center items-center">
             <h1 className="purple text-center">Bienvenue</h1>
-            <h3 className="text-center w-full px-2 max-w-2xl self-center">Cette plateforme vous permet d'enregistrer et de gérer vos dettes et vos créances, aussi suivre vos paiements et visualiser votre progression.</h3>
+            <h3 className="text-center w-full px-2 max-w-2xl self-center">Cette plateforme vous permet d'enregistrer et de gérer vos dettes et vos créances, ainsi que de suivre vos paiements et visualiser votre progression.</h3>
             <CoinAnimation />
             <CallToAction ctActions={ctActions} />
         </div>
