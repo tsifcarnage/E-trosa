26ceaca37dcb068041d7d1f040b8740c56ceb8ba
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-04-08 15:29:07 +0200

    update

--- src/pages/Dettes.tsx
+++ src/pages/Dettes.tsx
@@ -3,8 +3,15 @@ import Card from "../components/Card"
 import type { ICardGrad } from "../models/ui.interfaces"
 import { MOCK_DEBT } from "../data/debts.mock";
 import type { IDebts } from "../models/debts.interfaces";
-import { statusBadge } from "../utils/debts.logic";
+// import { statusBadge } from "../utils/debts.logic";
 
+
+import { AgGridReact } from "ag-grid-react";
+import type { ColDef } from "ag-grid-community";
+import { ModuleRegistry, AllCommunityModule, themeQuartz } from 'ag-grid-community';
+
+// On enregistre TOUTES les fonctionnalités communautaires d'un coup
+ModuleRegistry.registerModules([AllCommunityModule]);
 const cardDette: ICardGrad[] = [
     { title: "Total dettes", label: 12450, unit: "€", color: "text-red-500", grad: "red-card-grad" },
     { title: "Montant à payer", label: 5300, unit: "€", color: "text-green-500", grad: "green-card-grad" },
@@ -12,10 +19,25 @@ const cardDette: ICardGrad[] = [
     { title: "Créanciers", label: 8, unit: "Créanciers", color: "text-blue-300", grad: "blue-card-grad" },
 ]
 function Dettes() {
-    const [debts, setDebts] = useState<IDebts[]>([]);
+    const [debtsRow, setDebtsRow] = useState<IDebts[]>([]);
+    const [debtsCol] = useState<ColDef<IDebts>[]>([
+        { field: "creditor", headerName: "Créancier", flex: 1, headerClass: 'header-center' },
+        { field: "debtAmount", headerName: "Montant", flex: 1, headerClass: 'header-center' },
+        { field: "remainingAmount", headerName: "Restant", flex: 1, headerClass: 'header-center' },
+        { field: "status", headerName: "Status", flex: 1, headerClass: 'header-center' },
+        { field: "actions", headerName: "Actions", flex: 1, headerClass: 'header-center' },
+    ])
+    const themeAgGrid = themeQuartz.withParams({
+        backgroundColor: "#0f172a",
+        headerBackgroundColor: "#1e293b",
+        foregroundColor: "white",
+        fontSize: 14,
+        headerFontSize: 16,
+        fontFamily: "Inter, sans-serif"
+    });
 
     useEffect(() => {
-        setDebts(MOCK_DEBT);
+        setDebtsRow(MOCK_DEBT);
     }, []);
 
     return (
@@ -23,34 +45,12 @@ function Dettes() {
             <div>
                 <Card cards={cardDette} />
             </div>
-            <div className="grid gap-6 my-3">
-                {debts.map((debt, index) => (
-                    <div key={index} className="card bg-base-200 shadow-xl border border-base-300">
-                        <div className="card-body flex-row justify-between items-center">
-                            <div>
-                                <h3 className="card-title text-xl">{debt.creditor}</h3>
-                                <p className="text-sm opacity-60 font-medium">Échéance : {debt.dueDate}</p>
-                                <div className="mt-2">
-                                    {debt.status && (
-                                        <span className={`font-medium ${statusBadge(debt.status)}`}>
-                                            {debt.status}
-                                        </span>
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="text-right">
-                                <div className="text-xs opacity-50 uppercase font-bold">Reste à payer</div>
-                                <div className="text-2xl font-mono font-black text-secondary">
-                                    {debt.remainingAmount} €
-                                </div>
-                                <div className="text-xs opacity-70">
-                                    Initial : {debt.debtAmount}€ (+{debt.interestAmount}€ intérêts)
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+            <div className="h-80 text-center w-full max-w-300 m-auto " >
+                <AgGridReact
+                    theme={themeAgGrid}
+                    rowData={debtsRow}
+                    columnDefs={debtsCol}
+                />
             </div>
         </>
     )
