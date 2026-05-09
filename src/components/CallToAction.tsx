e64e8fa863e1e979a4d1084fe135e03e14c9166e
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-05-09 13:01:55 +0200

    update

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -6,7 +6,7 @@ export default function CallToAction({ ctActions = [] }: ICallToActionProps) {
         <section className="flex justify-center gap-5 flex-wrap max-w-300 m-5">
             {ctActions.map((ctAction, index) => (
                 <Link to={ctAction.to} className=" z-0 flex gap-5 w-full max-w-125 p-8 bg-neutral box-shad-Action rounded-lg scale-100 hover:scale-101" key={index}>
-                    <span className="self-center purple ">
+                    <span className="self-center text-secondary ">
                         {ctAction.icon}
                     </span>
                     <div className="flex flex-col gap-1 text-amber-50">
