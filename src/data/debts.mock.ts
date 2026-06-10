6d53c4c84e2bf56d57e68a2a937a74063b081dba
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-06-10 18:22:13 +0200

    Update debts.mock.ts

--- src/data/debts.mock.ts
+++ src/data/debts.mock.ts
@@ -8,10 +8,10 @@ export const MOCK_DEBT: IDebts[] = [
   createDebt("Kaka", 7000, 0, 5, "2026-07-27"),
   createDebt("Tony", 500, 400, 5, "2026-04-27"),
   createDebt("Serge", 6000, 1000, 5, "2026-04-27"),
-  createDebt("Cafard", 5000, 0, 5, "2024-04-27"),
+  createDebt("Cafard", 5000, 0, 5, "2024-10-27"),
   createDebt("Miggles", 8000, 100, 5, "2026-06-27"),
-  createDebt("Fifi", 50, 50, 3, "2026-04-27"),
-  createDebt("Riri", 50, 50, 3, "2026-04-27"),
+  createDebt("Fifi", 50, 50, 3, "2026-09-27"),
+  createDebt("Riri", 50, 50, 3, "2026-07-27"),
   createDebt("Loulou", 50, 50, 3, "2026-04-27"),
 ];
 
