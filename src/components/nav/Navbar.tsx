afab2ea6bfa024bc49614e067ea3258d3dff4dba
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-06-22 18:05:09 +0200

    Update Navbar.tsx

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar({ links }: ISidebarProps) {
 
           {isSummaryOpen && (
             <ul className="  dropdown-content bg-neutral btn-neutral box-shad-user w-full rounded-b-2xl text-center p-2 absolute -z-1 top-6 left-0">
-              <li className=" pt-4 text-[#c9cbd0] hover:text-[#3bf538c8] border-b border-b-[#8a38f5c8] py-2 cursor-pointer">
+              <li className=" pt-4 text-[#c9cbd0] hover:text-success border-b border-b-[#8a38f5c8] py-2 cursor-pointer">
                 <Link to={"/parametre"} onClick={handleClick}>
                   Voir Profil
                 </Link>
