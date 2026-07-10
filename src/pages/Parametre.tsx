98daa951a1315be2652cd103acac5f33390a339e
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-07-10 15:58:26 +0200

    Update Parametre.tsx

--- src/pages/Parametre.tsx
+++ src/pages/Parametre.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { supabase } from "../utils/supabaseClient";
 import { useEffect, useState } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
