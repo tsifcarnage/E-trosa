954d20dda9f75ce4fe5a7aacaf4550e7692b8ea8
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2026-07-17 18:01:54 +0200

    Update Error404.tsx

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -3,7 +3,7 @@ import ErrorAnimation from "../components/animationLottie/ErrorAnimation";
 
 export default function Error404() {
     return (
-        <div className="flex flex-col justify-start md:justify-center w-full md:m-auto text-error">
+        <div className="flex flex-col justify-start md:justify-center w-full my-20 md:my-0 text-error">
             <h1 className="self-center text-center">Erreur: Page non trouvée</h1>
             <ErrorAnimation />
             <Link className="btn btn-primary mx-5 lg:hidden" to={'/accueil'}>Accueil</Link>
